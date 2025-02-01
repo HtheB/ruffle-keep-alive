4254ee50e4246567d508789bef2b735e59d16290
HtheB <htheb61@gmail.com>
2025-02-01 03:58:48 +0100

    Update ruffle-keep-alive.user.js
    Updated the script to only apply to Ruffle pages, preventing unintended side effects on other websites.

--- ruffle-keep-alive.user.js
+++ ruffle-keep-alive.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Prevent Ruffle from Pausing
+// @name         Ruffle Keep Alive
 // @namespace    https://github.com/HtheB/ruffle-keep-alive
-// @version      1.0
-// @description  Prevents Ruffle from pausing when the tab is unfocused
+// @version      1.1
+// @description  Prevents Ruffle from pausing when switching tabs
 // @author       HtheB
 // @match        *://*/*
 // @run-at       document-start
@@ -11,24 +11,44 @@
 (function() {
     'use strict';
 
-    // Override the visibility API to always return "visible"
-    Object.defineProperty(document, "hidden", { get: () => false });
-    Object.defineProperty(document, "visibilityState", { get: () => "visible" });
+    function isRufflePage() {
+        return document.querySelector('ruffle-player') !== null;
+    }
 
-    // Function to dispatch focus & visibilitychange events only when needed
-    const keepActive = () => {
-        if (document.hidden) {
-            window.dispatchEvent(new Event("focus"));
-            document.dispatchEvent(new Event("visibilitychange"));
+    function keepRuffleActive() {
+        if (isRufflePage()) {
+            console.log("Ruffle detected - applying keep-alive fix");
+
+            // Override the visibility API only for Ruffle pages
+            Object.defineProperty(document, "hidden", { get: () => false });
+            Object.defineProperty(document, "visibilityState", { get: () => "visible" });
+
+            // Function to dispatch focus & visibilitychange events when needed
+            const keepActive = () => {
+                if (document.hidden) {
+                    window.dispatchEvent(new Event("focus"));
+                    document.dispatchEvent(new Event("visibilitychange"));
+                }
+            };
+
+            // Only fire events when necessary
+            document.addEventListener("visibilitychange", keepActive);
+            document.addEventListener("blur", keepActive);
+            window.addEventListener("blur", keepActive);
+            window.addEventListener("focus", keepActive);
+
+            // Also check every 5 seconds in case the browser prevents event dispatching
+            setInterval(keepActive, 5000);
         }
-    };
+    }
 
-    // Only fire events when necessary
-    document.addEventListener("visibilitychange", keepActive);
-    document.addEventListener("blur", keepActive);
-    window.addEventListener("blur", keepActive);
-    window.addEventListener("focus", keepActive);
+    // Wait for Ruffle to load before applying fix
+    const observer = new MutationObserver(() => {
+        if (isRufflePage()) {
+            keepRuffleActive();
+            observer.disconnect(); // Stop observing once Ruffle is found
+        }
+    });
 
-    // Also check every 5 seconds in case the browser prevents event dispatching
-    setInterval(keepActive, 5000);
+    observer.observe(document, { childList: true, subtree: true });
 })();
